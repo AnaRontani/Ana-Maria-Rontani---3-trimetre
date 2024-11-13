feb7f5a4bc4ee51ddfda9f83bce3205318515714
AnaRontani <ana.rontani@escola.pr.gov.br>
2024-11-13 11:27:33 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,120 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Ciencia',
+    '1.	O que são os recifes de corais?',
+    'Os recifes de corais são estruturas subaquáticas formadas por corais, pequenos animais marinhos que constroem esqueleto de calcário, formando habitats que abrigam muitas espécies.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Ciencia',
+    'Por que os golfinhos são considerados inteligentes?',
+    'Golfinhos são considerados inteligentes porque têm grande capacidade de aprendizado, comunicação e resolução de problemas. Eles também demonstram comportamentos sociais complexos.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Ciência',
+    'O que é plâncton?',
+    'Plâncton é um conjunto de organismos microscópicos que vivem na água e servem como alimento para muitos animais marinhos, como pequenos peixes e baleias.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Ciência',
+    'Por que as tartarugas marinhas põem ovos na praia?',
+    'As tartarugas marinhas põem ovos na praia para que os filhotes possam nascer fora da água e tenham condições seguras de crescer até estarem prontos para o oceano.'
+) 
+criaCartao(
+    'Ciência',
+    'O que é a bioluminescência?',
+    'Bioluminescência é a capacidade de alguns organismos de produzir luz, como certas espécies de peixes, águas-vivas e lulas, o que ajuda a se comunicar, atrair presas ou se proteger.'
 )
+
+criaCartao(
+    'Ciência',
+    'Qual é a função das barbatanas dos peixes?',
+    'As barbatanas ajudam os peixes a nadar, a se equilibrar e a mudar de direção na água.'
+)
+
+criaCartao(
+    'INTRNET',
+    'O que é a Hype House?',
+    'Uma mansão em Los Angeles onde influenciadores de TikTok moram e criam conteúdo juntos.'
+)
+
+criaCartao(
+    'INTRNET',
+    'Quem fundou a Hype House?',
+    'Fundada em 2019 por Thomas Petrou, Daisy Keech, Chase Hudson e Alex Warren.'
+)
+criaCartao(
+    'INTRNET',
+    'Quem fazia parte em 2021?',
+    'Alguns membros incluíam Alex Warren e Vinnie Hacker.'
+)
+
+criaCartao(
+    'INTRNET',
+    'Como ganha dinheiro?',
+    'Gera receita através de patrocínios e parcerias.'
+)
+
+criaCartao(
+    'INTRNET',
+    'Tem uma série na Netflix?',
+    'Sim, a série Hype House, que estreou no início de 2022, mostra o dia a dia dos membros.'
+)
+
+criaCartao(
+    'INTRNET',
+    'Por que é popular?',
+    'Ficou famosa pelo estilo de vida luxuoso e colaborações entre membros.'
+)
+criaCartao(
+    'geografia',
+    'Quantos países tem as Américas?',
+    '37'
+)
+criaCartao(
+    'INTRNET',
+    'Quais são os países que se estabelecem em mais de um continente?',
+    ' Russia e Turquia'
+)
+
+criaCartao(
+    'INTRNET',
+    'Qual a base do sistema capitalista?',
+    'O lucro'
+)
+criaCartao(
+    'INTRNET',
+    'Quantos fusos horários existem?',
+    '24'
+)
+
+criaCartao(
+    'INTRNET',
+    'Qual o rio mais longo do mundo?',
+    'Rio Nilo'
+)
+
+criaCartao(
+    'INTRNET',
+    'Qual o lugar com mais profundidade na terra?',
+    'As Fossas Marianas'
+)
+
+criaCartao(
+    'INTRNET',
+    'Quantos países tem as Américas?',
+    '37'
+)
+1.	O que são os recifes de corais?
+Os recifes de corais são estruturas subaquáticas formadas por corais, pequenos animais marinhos que constroem esqueleto de calcário, formando habitats que abrigam muitas espécies.
+	2.	Por que os golfinhos são considerados inteligentes?
+Golfinhos são considerados inteligentes porque têm grande capacidade de aprendizado, comunicação e resolução de problemas. Eles também demonstram comportamentos sociais complexos.
+	3.	O que é plâncton?
+Plâncton é um conjunto de organismos microscópicos que vivem na água e servem como alimento para muitos animais marinhos, como pequenos peixes e baleias.
+	4.	Por que as tartarugas marinhas põem ovos na praia?
+As tartarugas marinhas põem ovos na praia para que os filhotes possam nascer fora da água e tenham condições seguras de crescer até estarem prontos para o oceano.
+	5.	O que é a bioluminescência?
+Bioluminescência é a capacidade de alguns organismos de produzir luz, como certas espécies de peixes, águas-vivas e lulas, o que ajuda a se comunicar, atrair presas ou se proteger.
+	6.	Qual é a função das barbatanas dos peixes?
+As barbatanas ajudam os peixes a nadar, a se equilibrar e a mudar de direção na água.
