514ef6425101eb4ecf749c1635a8f32e7f7bc477
AnaRontani <ana.rontani@escola.pr.gov.br>
2024-11-27 11:02:30 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -73,48 +73,98 @@ criaCartao(
     '37'
 )
 criaCartao(
-    'INTRNET',
+    'geografia',
     'Quais são os países que se estabelecem em mais de um continente?',
     ' Russia e Turquia'
 )
 
 criaCartao(
-    'INTRNET',
+    'geografia',
     'Qual a base do sistema capitalista?',
     'O lucro'
 )
 criaCartao(
-    'INTRNET',
+    'geografia',
     'Quantos fusos horários existem?',
     '24'
 )
 
 criaCartao(
-    'INTRNET',
+    'geografia',
     'Qual o rio mais longo do mundo?',
     'Rio Nilo'
 )
 
 criaCartao(
-    'INTRNET',
+    'geografia',
     'Qual o lugar com mais profundidade na terra?',
     'As Fossas Marianas'
 )
-
 criaCartao(
     'INTRNET',
-    'Quantos países tem as Américas?',
-    '37'
+    'Quem inventou a lâmpado?',
+    'Thomos Edison.'
 )
-1.	O que são os recifes de corais?
-Os recifes de corais são estruturas subaquáticas formadas por corais, pequenos animais marinhos que constroem esqueleto de calcário, formando habitats que abrigam muitas espécies.
-	2.	Por que os golfinhos são considerados inteligentes?
-Golfinhos são considerados inteligentes porque têm grande capacidade de aprendizado, comunicação e resolução de problemas. Eles também demonstram comportamentos sociais complexos.
-	3.	O que é plâncton?
-Plâncton é um conjunto de organismos microscópicos que vivem na água e servem como alimento para muitos animais marinhos, como pequenos peixes e baleias.
-	4.	Por que as tartarugas marinhas põem ovos na praia?
-As tartarugas marinhas põem ovos na praia para que os filhotes possam nascer fora da água e tenham condições seguras de crescer até estarem prontos para o oceano.
-	5.	O que é a bioluminescência?
-Bioluminescência é a capacidade de alguns organismos de produzir luz, como certas espécies de peixes, águas-vivas e lulas, o que ajuda a se comunicar, atrair presas ou se proteger.
-	6.	Qual é a função das barbatanas dos peixes?
-As barbatanas ajudam os peixes a nadar, a se equilibrar e a mudar de direção na água.
+criaCartao(
+    'Ciencia',
+    'A que temperaturo o viguo ferve?',
+    '100 C'
+)
+
+criaCartao(
+    'Ciencia',
+    'luais são os fases do luo?',
+    'Novo, crescente, cheio e minguanto .'
+)
+
+criaCartao(
+    'Ciência',
+    'quantos ossos temos no nosso corpo?',
+    ' 206.'
+)
+
+criaCartao(
+    'Ciência',
+    'lual o moios planeto do sistemo solos.?',
+    'júpites'
+) 
+criaCartao(
+    'geografia',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin'
+)
+
+criaCartao(
+    'Ciência',
+    'De onde é a invenção do chuveiro elétrico? ',
+    'Brasil '
+)
+
+criaCartao(
+    'geografia',
+    'Qual o menor país do mundo?',
+    'Vaticano
+)
+
+criaCartao(
+    'geografia',
+    'Qual o maior país do mundo?',
+    ' Rússia '
+)
+
+criaCartao(
+    'matematica',
+    'Quantas casas decimais tem o número pi?',
+    ' infinitas'
+)
+criaCartao(
+    'inglẽs',
+    'O que é a palavra “Legend” significa em português?',
+    'Legenda'
+)
+
+criaCartao(
+    'geografia',
+    'Qual a montanha mais alta do Brasil?',
+    'Pico da neblina'
+)
